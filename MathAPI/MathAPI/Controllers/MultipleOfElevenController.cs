64da10da354f400a7532f528838d025a7b59bebe
Guilherme Galassi Scarpitta <gui.gsc@gmail.com>
2020-10-05 17:25:05 -0300

    remove unnecessary usings

--- MathAPI/MathAPI/Controllers/MultipleOfElevenController.cs
+++ MathAPI/MathAPI/Controllers/MultipleOfElevenController.cs
@@ -1,11 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using MathAPI.Model;
+﻿using MathAPI.Model;
 using MathAPI.MultipleOf.Interfaces;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 
 namespace MathAPI.Controllers
 {
